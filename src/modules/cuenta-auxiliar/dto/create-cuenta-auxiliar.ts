694c48223acfda799184220001dcdb7432d47650
Sebastiandot <sebastiantorrezaponte479@gmail.com>
2026-01-15 14:04:08 -0400

    feat:cambiosauxiliar

--- src/modules/cuenta-auxiliar/dto/create-cuenta-auxiliar.ts
+++ src/modules/cuenta-auxiliar/dto/create-cuenta-auxiliar.ts
@@ -1,7 +1,19 @@
+// src/cuenta-auxiliar/dto/create-cuenta-auxiliar.dto.ts
+import { IsString, IsNumber, IsBoolean, IsNotEmpty } from 'class-validator';
+
 export class CreateCuentaAuxiliarDto {
-  id_cuenta_auxiliar: number;
+  @IsNotEmpty()
+  @IsString()
   codigo: string;
+
+  @IsNotEmpty()
+  @IsString()
   nombre: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   id_empresa: number;
+
+  @IsBoolean()
   activo: boolean;
 }
--- src/modules/cuenta-auxiliar/dto/update-cuenta-auxiliar.ts
+++ src/modules/cuenta-auxiliar/dto/update-cuenta-auxiliar.ts
@@ -1,7 +1,21 @@
+import { IsString, IsNumber, IsBoolean, IsOptional } from 'class-validator';
+
 export class UpdateCuentaAuxiliarDto {
-  id_cuenta_auxiliar?: number;
+  // NO incluyas el ID aquí si lo pasas por la URL (:id)
+
+  @IsOptional()
+  @IsString()
   codigo?: string;
+
+  @IsOptional()
+  @IsString()
   nombre?: string;
+
+  @IsOptional()
+  @IsNumber()
   id_empresa?: number;
+
+  @IsOptional()
+  @IsBoolean()
   activo?: boolean;
 }
