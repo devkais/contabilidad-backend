d924fa968e9d0ce0a125f8760c4178f24bb994b9
Miguel Angel <calderontrujillo000@gmail.com>
2026-02-05 13:22:19 -0400

    feat: empresa-2.0

--- src/modules/empresa/empresa.module.ts
+++ src/modules/empresa/empresa.module.ts
@@ -4,9 +4,13 @@ import { Empresa } from './empresa.entity';
 import { EmpresaController } from './empresa.controller';
 import { EmpresaService } from './empresa.service';
 import { UsuarioEmpresaGestionModule } from '../usuario-empresa-gestion/usuario-empresa-gestion.module';
+import { Gestion } from '../gestion/gestion.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Empresa]), UsuarioEmpresaGestionModule],
+  imports: [
+    TypeOrmModule.forFeature([Empresa, Gestion]),
+    UsuarioEmpresaGestionModule,
+  ],
   providers: [EmpresaService],
   controllers: [EmpresaController],
   exports: [EmpresaService], // Exportamos el servicio para otros módulos
